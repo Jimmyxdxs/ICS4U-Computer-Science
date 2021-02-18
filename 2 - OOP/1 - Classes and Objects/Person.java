13307919e3de85dffa95249f828249ec2d3cf87d
Daniel Zhang <45671923+danielrzhang@users.noreply.github.com>
2021-02-17 21:48:52 -0500

    Add files via upload

---
+++ 2 - OOP/1 - Classes and Objects/Person.java
@@ -0,0 +1,15 @@
+class Person {
+  String firstName, lastName, address, postalCode, phoneNumber, email;
+  
+  Person() {}
+  
+  Person(String firstName, String lastName, String address, String postalCode, String phoneNumber, String email) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.address = address;
+    this.postalCode = postalCode;
+    this.phoneNumber = phoneNumber;
+    this.email = email;
+  }
+
+}
