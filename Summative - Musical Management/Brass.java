c615e3af342eb1eecadab3c57af6bc06ac92cad7
Daniel Zhang <45671923+danielrzhang@users.noreply.github.com>
2021-02-17 21:29:28 -0500

    Delete Summative - Musical Management directory

--- Summative - Musical Management/Brass.java
+++
@@ -1,9 +0,0 @@
-/**
- * Brass.java
- * represents a brass instrument that inherits from Wind, and is a superclass of Trumpet, Trombone, and Tuba.
- * @author Daniel Zhang
- * @since October 20, 2020
- * @version 1.0
- */
-abstract class Brass extends Wind {
-}
