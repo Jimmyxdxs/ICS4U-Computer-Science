0423e448f5fe94281820e3fde4bace326675546c
Daniel Zhang <45671923+danielrzhang@users.noreply.github.com>
2021-02-17 21:30:38 -0500

    Delete 4 - Data Structures directory

--- 4 - Data Structures/1 - ArrayLists/FrequencySort.java
+++
@@ -1,7 +0,0 @@
-import java.util.Comparator;
-
-class FrequencySort implements Comparator<LetterFrequency> {
-  public int compare(LetterFrequency lf1, LetterFrequency lf2) {
-    return lf2.getCount() - lf1.getCount();
-  }
-}
